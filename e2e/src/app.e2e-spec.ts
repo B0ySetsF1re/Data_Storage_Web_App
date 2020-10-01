03bd0fc42e2e8907a1302e832447e8364f382447
B0ySetsF1re <montersberg@gmail.com>
2020-10-01 16:28:32 +0300

    changed project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('data-storage-web-app app is running!');
+    expect(page.getTitleText()).toEqual(' app is running!');
   });
 
   afterEach(async () => {
