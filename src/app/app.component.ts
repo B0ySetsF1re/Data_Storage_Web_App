79cb3997935757444a23de267a3e49071e30c366
B0ySetsF1re <montersberg@gmail.com>
2020-10-07 20:37:50 +0300

    applied "bs-custom-file-input module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import bsCustomFileInput from 'bs-custom-file-input';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '';
+
+  ngOnInit() {
+    bsCustomFileInput.init();
+  }
 }
