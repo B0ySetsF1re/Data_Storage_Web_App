c173e42e57d1e9775c119f02ea0ffc093a9df268
B0ySetsF1re <montersberg@gmail.com>
2020-10-07 23:38:35 +0300

    started "upload_file.service" implementation

--- src/app/components/upload_file/upload_file.component.ts
+++ src/app/components/upload_file/upload_file.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Directive, HostListener } from '@angular/core';
 
+import { UploadFileService } from '../../services/upload_file/upload_file.service';
+
 @Component({
   selector: 'upload-file',
   templateUrl: './upload_file.component.html',
-  styleUrls: ['./upload_file.component.css']
+  styleUrls: ['./upload_file.component.css'],
+  providers: [UploadFileService]
 })
 export class UploadFileComponent {
   title = '';
---
+++ src/app/services/upload_file/upload_file.service.ts
@@ -0,0 +1,7 @@
+//import { Directive, HostListener } from '@angular/core';
+import { Injectable } from '@angular/core'
+
+@Injectable()
+export class UploadFileService {
+  
+}
