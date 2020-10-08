011e568a4ebfcc453326ea067bc349029329e85b
B0ySetsF1re <montersberg@gmail.com>
2020-10-09 01:34:34 +0300

    marked optional prioperties

--- src/app/components/files-metadata/file-metadata.ts
+++ src/app/components/files-metadata/file-metadata.ts
@@ -1,8 +1,9 @@
 export class fileMetaData {
   object_id: string;
   file_name: string;
+  disposition?: string;
   length: string;
-  type: string;
+  type?: string;
   extension: string;
   upload_date: string;
   upload_time: string;
