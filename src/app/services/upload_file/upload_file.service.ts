ee918a906425c15c331f6ce758425aa26a2826be
B0ySetsF1re <montersberg@gmail.com>
2020-10-08 16:15:34 +0300

    removed old implementation as it needs new approach

--- src/app/services/upload_file/upload_file.service.ts
+++ src/app/services/upload_file/upload_file.service.ts
@@ -1,19 +1,18 @@
-//import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest, HttpEvent } from '@angular/common/http';
+//import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class UploadFileService {
+  private baseUrl = 'http://localhost:3000/api/data-storage';
+
   constructor(
     private http: HttpClient) { };
 
-  uploadFile() {
-    let headers = new HttpHeaders();
-
-    headers.append('Content-Type', 'multipart/form-data');
-    return this.http.post('http://localhost:3000/api/data-storage/upload-file', { headers: headers })
-      .pipe(map(res => res));
+  uploadFile(file: File) { // : Observable<HttpEvent<any>>
+    
   }
 
 }
