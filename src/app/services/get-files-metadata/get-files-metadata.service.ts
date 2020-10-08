867897d4a99f04fbbde2ca0e6aff7b12dba0df7b
B0ySetsF1re <montersberg@gmail.com>
2020-10-08 21:40:21 +0300

    implemented "GetFilesMetadataService"

--- src/app/services/get-files-metadata/get-files-metadata.service.ts
+++ src/app/services/get-files-metadata/get-files-metadata.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
-import { fileMetaData } from './file-metadata';
+import { HttpClient } from '@angular/common/http'; // HttpHeaders, HttpRequest, HttpEvent
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetFilesMetadataService {
+  private baseUrl = 'http://localhost:3000/api/data-storage';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getFilesMetaData() {
+    return this.httpClient.get<any>(`${this.baseUrl}/meta-data-content`);
+  }
 }
