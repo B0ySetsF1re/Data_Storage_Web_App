8e84cc36180db114d4d4f990d8258eedd4aa72c7
B0ySetsF1re <montersberg@gmail.com>
2020-10-09 20:45:36 +0300

    implemented "files-stats" service

--- src/app/components/files-stats/files-stats.component.ts
+++ src/app/components/files-stats/files-stats.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FilesStatsService } from '../../services/files-stats/files-stats.service';
+
 @Component({
   selector: 'app-files-stats',
   templateUrl: './files-stats.component.html',
-  styleUrls: ['./files-stats.component.css']
+  styleUrls: ['./files-stats.component.css'],
+  providers: [FilesStatsService]
 })
 export class FilesStatsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private filesStatsService: FilesStatsService) {
+    this.getFilesStats();
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  getFilesStats() {
+    this.filesStatsService.getFilesStats()
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+        }
+      )
   }
 
 }
--- src/app/services/files-stats/files-stats.service.ts
+++ src/app/services/files-stats/files-stats.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilesStatsService {
+  private baseUrl = 'http://localhost:3000/api/data-storage/files-stats';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getFilesStats() {
+    return this.httpClient.get<any>(`${this.baseUrl}`, { });
+  }
 }
