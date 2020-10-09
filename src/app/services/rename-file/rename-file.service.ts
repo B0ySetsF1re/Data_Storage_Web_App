0ea6d7d7572ec8040deb9ba5356eab6933edf4db
B0ySetsF1re <montersberg@gmail.com>
2020-10-09 19:59:39 +0300

    implemented "rename-file" service

--- src/app/services/rename-file/rename-file.service.ts
+++ src/app/services/rename-file/rename-file.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RenameFileService {
+  private baseUrl = 'http://localhost:3000/api/data-storage/rename-uploaded-file/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  renameFile(id: string, body: any) {
+    return this.httpClient.post<any>(`${this.baseUrl}${id}`, body);
+  }
 }
