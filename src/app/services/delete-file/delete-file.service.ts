659e23f69cba42b955578ffa8a9bf0d5bd6f4159
B0ySetsF1re <montersberg@gmail.com>
2020-10-09 04:02:19 +0300

    implemented delete service, but it needs further enhancement

--- src/app/services/delete-file/delete-file.service.ts
+++ src/app/services/delete-file/delete-file.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeleteFileService {
+  private baseUrl = 'http://localhost:3000/api/data-storage/delete-uploaded-file/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  deleteFile(id: any) {
+    return this.httpClient.post<any>(`${this.baseUrl}/${id}`);
+  }
 }
