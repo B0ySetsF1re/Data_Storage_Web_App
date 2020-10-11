8ecd96db876d7505e524a6141fa7c022ec51b52b
B0ySetsF1re <montersberg@gmail.com>
2020-10-11 11:52:20 +0300

    made "upload-file" service as a parent and provider for other children components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import bsCustomFileInput from 'bs-custom-file-input';
 
+import { UploadFileService } from './services/upload-file/upload-file.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UploadFileService]
 })
 export class AppComponent {
   title = '';
