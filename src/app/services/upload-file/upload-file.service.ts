dc5cd8019d32024377549e342c688c237ea8b935
B0ySetsF1re <montersberg@gmail.com>
2020-10-11 11:54:01 +0300

    now "upload-service" trigers event about file being uploaded by some certain component in the family tree

--- src/app/services/upload-file/upload-file.service.ts
+++ src/app/services/upload-file/upload-file.service.ts
@@ -1,16 +1,20 @@
-import { Injectable } from '@angular/core'
+import { Injectable, Output, EventEmitter } from '@angular/core'
 import { HttpClient } from '@angular/common/http'; // HttpHeaders, HttpRequest, HttpEvent
 // import { Observable } from 'rxjs';
 // import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class UploadFileService {
   private baseUrl = 'http://localhost:3000/api/data-storage';
+  private fileUploadedSource = new Subject<any>(); // Observable source of any type
+
+  fileUploaded$ = this.fileUploadedSource.asObservable(); // Observable stream of any type
 
   constructor(private httpClient: HttpClient) { };
 
   uploadFile(formData: FormData) { // : Observable<HttpEvent<any>>
+    this.fileUploadedSource.next();
     return this.httpClient.post<any>(`${this.baseUrl}/upload-file`, formData);
   }
-
 }
