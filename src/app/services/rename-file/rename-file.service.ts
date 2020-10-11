68c7d0e9816d3329133dfde6fa1a40750f5747d7
B0ySetsF1re <montersberg@gmail.com>
2020-10-11 15:16:51 +0300

    now "rename-file" service can be subscribed from other components to check if file was renamed

--- src/app/services/rename-file/rename-file.service.ts
+++ src/app/services/rename-file/rename-file.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RenameFileService {
   private baseUrl = 'http://localhost:3000/api/data-storage/rename-uploaded-file/';
+  private fileRenamedSource = new Subject<any>();
+
+  fileRenamed$ = this.fileRenamedSource.asObservable();
 
   constructor(private httpClient: HttpClient) { }
 
   renameFile(id: string, body: any) {
-    return this.httpClient.post<any>(`${ this.baseUrl + id}`, body);
+    return this.httpClient.post<any>(`${ this.baseUrl + id}`, body)
+      .pipe(
+        tap(
+          res => {
+            this.fileRenamedSource.next(res);
+          },
+          err => {
+            this.fileRenamedSource.next(err);
+          }
+        )
+      );
   }
 }
