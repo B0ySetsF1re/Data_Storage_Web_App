cb744504db6e2195e381a4d9c4f327975dfa88f1
B0ySetsF1re <montersberg@gmail.com>
2020-10-11 13:20:41 +0300

    now main app component is provider of "rename-file" and "delete-file" services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import bsCustomFileInput from 'bs-custom-file-input';
 
 import { UploadFileService } from './services/upload-file/upload-file.service';
+import { DeleteFileService } from './services/delete-file/delete-file.service';
+import { RenameFileService } from './services/rename-file/rename-file.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [UploadFileService]
+  providers: [UploadFileService, DeleteFileService, RenameFileService]
 })
 export class AppComponent {
   title = '';
